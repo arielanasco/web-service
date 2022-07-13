9d904425145275b931a917fb8384a53c7a391a71
arielanasco <ariel@linkmind.co.jp>
2022-07-13 13:49:56 +0800

    Update

--- decompress/views.py
+++ decompress/views.py
@@ -23,34 +23,31 @@ def index(request):
     print(client_key)
 
     if client_key in settings.CLIENT_KEY:
-        try:
-            if not url:
-                raise Exception("URL should not be empty.") 
+        if not url:
+            raise Exception("URL should not be empty.") 
 
-            with requests.get(url, stream=True) as data:
-                if data.status_code != 200:
-                    raise Exception("File not found on the server") 
+        with requests.get(url, stream=True) as data:
+            if data.status_code != 200:
+                raise Exception("File not found on the server") 
 
-                data_headers = data.headers.get('content-disposition')
-                filename = re.findall('filename=(.+)', data_headers)[0]
-                is_valid_file = filename.endswith('gz',-3,-1)
+            data_headers = data.headers.get('content-disposition')
+            filename = re.findall('filename=(.+)', data_headers)[0]
+            is_valid_file = filename.endswith('gz',-3,-1)
 
-                with gzip.open("temp.log.gz", "wb") as file:
-                    for chunk in data.iter_content(chunk_size=16*1024):
-                        file.write(data.content) 
+            with gzip.open("temp.log.gz", "wb") as file:
+                for chunk in data.iter_content(chunk_size=16*1024):
+                    file.write(data.content) 
 
-            if not is_valid_file:
-                os.remove("temp.log.gz")
-                raise Exception("File should be gzip only")
-
-            with gzip.open("temp.log.gz", 'rb') as ip:
-                    with io.TextIOWrapper(ip, encoding='utf-8') as decoder:
-                        content = decoder.read()
-
+        if not is_valid_file:
             os.remove("temp.log.gz")
-            return HttpResponse(f"{content}",content_type="text/plain", status=status.HTTP_200_OK)
-        except Exception as err:
-                return Response({"error":f'{err}'},status=status.HTTP_400_BAD_REQUEST)
+            raise Exception("File should be gzip only")
+
+        with gzip.open("temp.log.gz", 'rb') as ip:
+                with io.TextIOWrapper(ip, encoding='utf-8') as decoder:
+                    content = decoder.read()
+
+        os.remove("temp.log.gz")
+        return HttpResponse(f"{content}",content_type="text/plain", status=status.HTTP_200_OK)
 
     else:
         return Response({"error":f'Invalid Key Provided'},status=status.HTTP_400_BAD_REQUEST)
