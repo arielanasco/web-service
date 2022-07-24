236392681734082d4dd6c3f8f52bf8b3ade1865a
arielanasco <ariel@linkmind.co.jp>
2022-07-25 07:45:33 +0800

    Updates

--- decompress/admin.py
+++ decompress/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models import TestFile
+from django.conf import settings
 
 
 admin.site.register(TestFile)
+
+admin.site.site_header = f"{settings.SITE_NAME} Portal"
+admin.site.site_title = f"{settings.SITE_NAME} Admin"
+admin.site.index_title = f"{settings.SITE_NAME} Dashboard"
